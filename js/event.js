dba99a3eee2c430f436e8a104dd6c406c196ad24
ppotatoG <thgus2270@gmail.com>
2021-03-02 22:47:22 +0900

    edit06

--- js/event.js
+++ js/event.js
@@ -17,3 +17,37 @@ function headerFunction() {
 function MenuToggle(){
     document.querySelector('.menu-toggle').classList.toggle('on')
 }
+
+//resize, onload 마다 img.alt 속성을 바꿔주나요? 그럼 같은 내용 함수를 두번?
+//클릭마다 this 잡기
+//click 섹션마다 다르게 들어가야 해??? ?????
+
+/*
+딜레이{
+    click, this z-index: + margin
+    delay, this(not) z-index : -
+}
+*/
+
+let tabBtn=document.querySelectorAll('.tab-menu li'),
+    tabImg=document.querySelectorAll('.inner-contents img');
+
+for(let i=0; i<tabBtn.length; i++){
+    tabBtn[i].addEventListener('click', function(){
+        console.log(i)
+        console.log(tabBtn[i])
+        console.log(tabImg[i])
+
+        tabBtn[i].classList.remove('on')
+        tabImg[i].classList.remove('on')
+
+        const dataSet=this.dataset.tabmsg
+
+        if(this.classList!=="on"){
+            this.classList.add('on')
+            document.querySelector('.'+dataSet).classList.add('on')
+        }
+        
+    })
+}
+//중복 꼬엿다!
