5864ad83c44021336f96523156e5b2843ca0d42d
ppotaoG <thgus2270@gmail.com>
2021-04-05 17:54:43 +0900

    공부

--- js/event.js
+++ js/event.js
@@ -1,29 +1,29 @@
-let header = document.querySelector("header"),
-    sticky = header.offsetTop,
-    MobileCTA=document.querySelector('#moblie-cta');
+// let header = document.querySelector("header"),
+//     sticky = header.offsetTop,
+//     MobileCTA=document.querySelector('#moblie-cta');
 
-//onscroll은 뭐죠 addEventListenr랑 달라여?
-function headerFunction() {
-    if (window.pageYOffset > sticky) {
-    header.classList.add("fix")
-    MobileCTA.style.bottom=0+"px"
-    } else {
-    header.classList.remove("fix");
-    MobileCTA.style.bottom=60+"px"
-    }
-}
+// //onscroll은 뭐죠 addEventListenr랑 달라여?
+// function headerFunction() {
+//     if (window.pageYOffset > sticky) {
+//     header.classList.add("fix")
+//     MobileCTA.style.bottom=0+"px"
+//     } else {
+//     header.classList.remove("fix");
+//     MobileCTA.style.bottom=60+"px"
+//     }
+// }
 
-window.onscroll = function() {headerFunction()};
+// window.onscroll = function() {headerFunction()};
 
-window.addEventListener('load', ()=>{
-    if (window.pageYOffset > sticky) {
-        header.classList.add("fix")
-        MobileCTA.style.bottom=0+"px"
-    } else {
-        header.classList.remove("fix");
-        MobileCTA.style.bottom=60+"px"
-    }
-});
+// window.addEventListener('load', ()=>{
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("fix")
+//         MobileCTA.style.bottom=0+"px"
+//     } else {
+//         header.classList.remove("fix");
+//         MobileCTA.style.bottom=60+"px"
+//     }
+// });
 
 function MenuToggle(){
     document.querySelector('.menu-toggle').classList.toggle('on')
@@ -40,26 +40,46 @@ function MenuToggle(){
 }
 */
 
-let tabBtn=document.querySelectorAll('.tab-menu li'),
+let tabBtns=document.querySelectorAll('.tab-menu li'),
     tabImg=document.querySelectorAll('.inner-contents img'),
     tabmessage=document.querySelector('.tab-area .tab-message');
 
-for(let i=0; i<tabBtn.length; i++){
-    tabBtn[i].addEventListener('click', function(){
-        for(let n=0; n<tabBtn.length; n++){
-            tabBtn[n].classList.remove('on')
-            tabImg[n].classList.remove('on')
-        }
-        const dataSet=this.dataset.tabmsg
-        if(this.classList!=="on"){
-            this.classList.add('on')
-            document.querySelector('.'+dataSet).classList.add('on')
-        }
-        if(dataSet=='contacts') tabmessage.textContent="계좌번호를 몰라도 연락처만으로 송금할 수 있습니다."
-        else if(dataSet=='sagi') tabmessage.textContent="송금하는 계좌에 피해 사례가 있는지 확인할 수 있습니다."
-        else if(dataSet=='message') tabmessage.textContent="받는분에게 마음을 전해보세요"
-    })
+// for(let i=0; i<tabBtn.length; i++){
+//     tabBtn[i].addEventListener('click', function(){
+//         for(let n=0; n<tabBtn.length; n++){
+//             tabBtn[n].classList.remove('on')
+//             tabImg[n].classList.remove('on')
+//         }
+//         const dataSet=this.dataset.tabmsg
+//         if(this.classList!=="on"){
+//             this.classList.add('on')
+//             document.querySelector('.'+dataSet).classList.add('on')
+//         }
+//         if(dataSet=='contacts') tabmessage.textContent="계좌번호를 몰라도 연락처만으로 송금할 수 있습니다."
+//         else if(dataSet=='sagi') tabmessage.textContent="송금하는 계좌에 피해 사례가 있는지 확인할 수 있습니다."
+//         else if(dataSet=='message') tabmessage.textContent="받는분에게 마음을 전해보세요"
+//     })
+// }
+// console.dir(tabBtns)
+
+// for( const tabBtn of tabBtns ){
+//     tabBtn.addEventListener('click', function(){
+//     })
+// }
+let Wrap = document.querySelector('#wrap')
+Wrap.addEventListener('click', function(e){
+    console.dir(e)
+    if( e.target.li ) console.log('ddd')
+})
+
+// for( const tabBtn of tabBtns ){
+//     console.log(tabBtn)
+//     console.log(tabBtns)
+// }
+for( const i = tabBtns; i > tabBtns.length; i++){
+    console.log(tabBtns[i])
 }
+
 $('.counter-20').counterUp({
     delay: 10,
     time: 2000
