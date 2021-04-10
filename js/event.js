185903345f53536118648d15b6bad806561a8b3b
ppotatoG <thgus2270@gmail.com>
2021-04-11 00:34:32 +0900

    section 추가(inquiry, card) class, id 변경

--- js/event.js
+++ js/event.js
@@ -67,6 +67,15 @@ contents.addEventListener('click', function(e){
             case ('mini') :
                 tabMsg.textContent='잠깐씩 필요한 보험부터 삶에 꼭 필요한 보장성 보험까지.나에게 필요한 미니보험을 찾아보세요.';
                 break;
+            case ('view') :
+                tabMsg.textContent='복잡한 과정 없이 무료로 신용점수를 확인할 수 있습니다.';
+                break;
+            case ('broker') :
+                tabMsg.textContent='대출 상품을 간편하게 비교해보고 꼭 맞는 대출을 찾을 수 있습니다.';
+                break;
+            case ('cardrecommend') :
+                tabMsg.textContent='내게 맞는 혜택을 제공하는 신용카드를 추천 받을 수 있습니다..';
+                break;
             default :
                 false;
         }
