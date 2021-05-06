8a92156764ed46dc1bc5101ccaf92752b44e4942
ppotaoG <thgus2270@gmail.com>
2021-05-06 18:44:29 +0900

    message 제외 tab 완료

--- js/event.js
+++ js/event.js
@@ -43,43 +43,21 @@ menuBtn.addEventListener('click', function(){
 });
 
 
-document.addEventListener('click', function(e){
-    if(e.target.dataset.tab == 'true') {
-        let elId = e.target.id
-        let parentNode = document.querySelector('#'+elId)
-        let tabLists = parentNode.querySelectorAll('.menu li')
-        let tabImgs = parentNode.querySelectorAll('.inner-contents img')
-        let tabMessages = parentNode.querySelectorAll('.tab-message')
-    }
-})
+let tabBtn = document.querySelectorAll('#annoying .menu li')
+let tabImg = document.querySelectorAll('#annoying .inner-contents img')
 
-/*
 
-my
-나는 보험료를 얼마나 똑똑하게 내고 있을까? 내 보험, 조회와 진단까지 한 번에 해보세요.
+for(let i = 0; i < tabBtn.length; i++){
+    tabBtn[i].addEventListener('click', function(){
+        for(let n = 0; n < tabBtn.length; n++){
+            tabBtn[n].classList.remove('on')
+            tabImg[n].classList.remove('on')
 
-return
-귀찮아서 미뤄왔던 보험금 청구, 토스에서 간편하게 해보세요.
+            let thisDataset = this.dataset.tabName
+            let thisImg = document.querySelector('.'+thisDataset)
 
-mini
-잠깐씩 필요한 보험부터 삶에 꼭 필요한 보장성 보험까지.나에게 필요한 미니보험을 찾아보세요.
-
-view
-복잡한 과정 없이 무료로 신용점수를 확인할 수 있습니다.
-
-broker
-대출 상품을 간편하게 비교해보고 꼭 맞는 대출을 찾을 수 있습니다.
-
-cardrecommend
-내게 맞는 혜택을 제공하는 신용카드를 추천 받을 수 있습니다.
-
-dutchpay
-더치페이할 때도 직접 계한하지 말고 쉽게 주고받으세요.
-
-atm
-카드 없이도 ATM에서 출금할 수 있습니다.
-
-exchange
-최대 100% 환율 우대로 저렴하게 환전할 수 있습니다.
-
-*/
+            this.classList.add('on')
+            thisImg.classList.add('on')
+        }
+    })
+}
