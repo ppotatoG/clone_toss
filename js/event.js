3dd863cbe55f3057444cb71390af406241fd3c2a
ppotatoG <thgus2270@gmail.com>
2021-05-10 23:47:01 +0900

    js 정리

--- js/event.js
+++ js/event.js
@@ -49,34 +49,13 @@ menuBtn.addEventListener('click', function(){
     }
 })
 
-
-// let tabBtn = document.querySelectorAll('#annoying ul li')
-// let tabImg = document.querySelectorAll('#annoying .inner-contents img')
-// let tabMessage = document.querySelectorAll('#annoying .tab-message')
-
-// for(let i = 0; i < tabBtn.length; i++){
-//     tabBtn[i].addEventListener('click', function(){
-//         for(let n = 0; n < tabBtn.length; n++){
-//             tabBtn[n].classList.remove('on')
-//             tabImg[n].classList.remove('on')
-//             tabMessage[n].classList.remove('on')
-
-//             let thisDataset = this.dataset.tabName
-//             let thisImg = document.querySelector('.'+thisDataset)
-//             let thisMessage = document.querySelector('.'+thisDataset+'-msg')
-
-//             this.classList.add('on')
-//             thisImg.classList.add('on')
-//             thisMessage.classList.add('on')
-//         }
-//     })
-// }
-
 function tabFnc(e){
     const parent = e.parentNode.parentNode.parentNode.parentNode
+
     const tabImgs = parent.querySelectorAll('.inner-contents img')
     const tabBtns = parent.querySelectorAll('ul li')
     const tabMsgs = parent.querySelectorAll('.tab-message')
+
     const dataTab = e.dataset.tabName
     const dataSelect = parent.querySelector('.'+dataTab)
     const dataSelectMsg = parent.querySelector('.'+dataTab+'-msg')
