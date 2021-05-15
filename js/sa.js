f440cdfa58f9ab301a0a2009bed4a83abd1b31ed
ppotatoG <thgus2270@gmail.com>
2021-05-15 13:02:05 +0900

    sa 파일 추가

---
+++ js/sa.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', () => {
+    const saNodes = document.querySelectorAll('.box');
+    const point = window.innerHeight;
+    for ( const saNode of saNodes){
+        const rect = saNode.getBoundingClientRect();
+        if(rect.top <= point) {
+            saNode.classList.add('show');
+        } else if(rect.top > point + point*(0.1)) {
+            saNode.classList.remove('show');
+        } 
+    }
+});
